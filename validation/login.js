eef67f080cecaf81f0569797ff0fe387c69896a3
natnaelh14 <natnaelh14@github.com>
2021-08-14 02:24:53 -0400

    created log in validator

--- validation/login.js
+++ validation/login.js
@@ -1,17 +1,11 @@
 const Validator = require("validator");
 const isEmpty = require("is-empty");
 
-module.exports = function validateRegisterInput(input) {
+module.exports = function validateLoginInput(input) {
   let errors = {};
 // Convert empty fields to an empty string so we can use validator functions
-input.name = !isEmpty(input.name) ? input.name : "";
-input.email = !isEmpty(input.email) ? input.email : "";
-input.password = !isEmpty(input.password) ? input.password : "";
-input.password2 = !isEmpty(input.password2) ? input.password2 : "";
-// Name checks
-  if (Validator.isEmpty(input.name)) {
-    errors.name = "Name field is required";
-  }
+  input.email = !isEmpty(input.email) ? input.email : "";
+  input.password = !isEmpty(input.password) ? input.password : "";
 // Email checks
   if (Validator.isEmpty(input.email)) {
     errors.email = "Email field is required";
@@ -21,15 +15,6 @@ input.password2 = !isEmpty(input.password2) ? input.password2 : "";
 // Password checks
   if (Validator.isEmpty(input.password)) {
     errors.password = "Password field is required";
-  }
-if (Validator.isEmpty(input.password2)) {
-    errors.password2 = "Confirm password field is required";
-  }
-if (!Validator.isLength(input.password, { min: 8, max: 20 })) {
-    errors.password = "Password must be at least 8 characters";
-  }
-if (!Validator.equals(input.password, input.password2)) {
-    errors.password2 = "Passwords must match";
   }
 return {
     errors,
