241a81a9e4482229c98e5940e4c664ad9baa333b
natnaelh14 <natnaelh14@github.com>
2021-08-14 15:37:10 -0400

    created entry card components

---
+++ client/src/components/EntryCard/EntryCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledEntryCard } from "./styles";
+
+function EntryCard({ children }) {
+  return <StyledEntryCard>{children}</StyledEntryCard>;
+}
+
+export default EntryCard;
