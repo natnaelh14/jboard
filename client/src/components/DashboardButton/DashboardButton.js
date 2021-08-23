424c12c53be54ca72878b8ef8e87665e65be23d0
natnaelh14 <natnaelh14@github.com>
2021-08-22 22:37:07 -0400

    enabled resume button from user data

--- client/src/components/DashboardButton/DashboardButton.js
+++ client/src/components/DashboardButton/DashboardButton.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { HomeStyles, ButtonsWrapper, Button } from "./style";
+import { ButtonsWrapper, Button } from "./style";
+import Auth from "../../utils/auth";
 
 export default function Home() {
   return (
@@ -10,7 +11,7 @@ export default function Home() {
         </a>
       </Button>
       <Button>
-        <a href="#" class="animated-button thar-two">
+        <a href={Auth.getUser().data.resume_url} class="animated-button thar-two">
           RESUME
         </a>
       </Button>
