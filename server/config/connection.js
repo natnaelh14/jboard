9bcaf68393322804181c0c938f75a62e0d758b72
natnaelh14 <natnaelh14@github.com>
2021-08-23 08:58:24 -0400

    feat show mongoose connection error and success logs

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,16 @@
 
 const mongoose = require('mongoose');
+let data_url = process.env.MONGODB_URI || 'mongodb://localhost/jboard'
+console.log({data_url})
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/jboard', {
+mongoose.connect(data_url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
 
+
+mongoose.connection.once('open',()=> console.log('connection established'))
+.on('error',(error)=> console.log('establishing connection failed',error))
 module.exports = mongoose.connection;
