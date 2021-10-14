9ac7c4669ba69039c2a3cac5e3e7d6dcf43ad3c1
natnaelh14 <natnaelh14@github.com>
2021-10-14 16:19:42 -0400

    enabled scrolling in the columns

--- client/src/components/DragAndDrop/column.jsx
+++ client/src/components/DragAndDrop/column.jsx
@@ -26,7 +26,7 @@ const Title = styled.h3`
 const TaskList = styled.div`
   padding: 8px;
   transition: background-color 0.2s ease;
-  border: 3px solid #e8c8c5;
+  ${'' /* border: 3px solid pink; */}
   background-color: ${(props) =>
     props.isDraggingOver ? "#fff4d4" : "#fff7f7"};
   flex-grow: 1;
